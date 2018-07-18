e58ad0be18d9ce71c99372bec2f74a651a38f6bc
sg777 <sarath.ginjupalli89@gmail.com>
2018-07-18 12:13:29 +0530

    adding apis

--- privatebet/bet-cli.h
+++ privatebet/bet-cli.h
@@ -1,7 +1,7 @@
 #include "bet.h"
 
-void bet_player_deck_blind(struct pair256 *cards,struct pair256 key,int32_t n);
-bits256 bet_curve25519_rand256(int32_t privkeyflag,int8_t index);
 struct pair256 bet_player_create();
+void bet_player_deck_create(int n,struct pair256 *cards);
+bits256 bet_curve25519_rand256(int32_t privkeyflag,int8_t index);
 void bet_player_deck_blind(struct pair256 *cards,struct pair256 key,int32_t n);
 
