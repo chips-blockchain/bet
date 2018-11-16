11b8beaaa3afba70aada9fecc3e9eb37de3eaaf2
sg777 <sarath.ginjupalli89@gmail.com>
2018-11-16 13:18:37 +0100

    debugging

---
+++ privatebet/test.c
@@ -0,0 +1,16 @@
+/*#ifdef _cplusplus 
+extern "C" {
+#endif
+*/
+#include <stdio.h>
+int main(int argc, char **argv)
+{
+	printf("\nHello World");
+	my_bet(argc,argv);
+	return 0;
+}
+/*
+#ifdef _cplusplus
+}
+# endif
+*/
