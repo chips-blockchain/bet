56ed67ba4a8387cbc21ae9fc259d97654c61aa2a
chandram47 <chandram47@gmail.com>
2018-11-26 18:31:43 +0530

    testing

--- privatebet/client.c
+++ privatebet/client.c
@@ -1334,7 +1334,11 @@ int32_t BET_p2p_client_give_share(cJSON *argjson,struct privatebet_info *bet,str
 	uint8_t decipher[sizeof(bits256) + 1024],*ptr;
 	bits256 share;
 	
-	
+	if(playerid!=bet->myplayerid)
+		{
+			printf("\nDo Nothing in %s",__FUNCTION__);
+			return 1;
+		}
 	playerid=jint(argjson,"playerid");
 	cardid=jint(argjson,"cardid");
 
