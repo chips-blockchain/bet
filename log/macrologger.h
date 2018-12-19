ddb6a234d948b545844ac0134d4b576ba3558617
chandram47 <chandram47@gmail.com>
2018-12-19 17:46:06 +0530

    debugging

--- log/macrologger.h
+++ log/macrologger.h
@@ -64,8 +64,7 @@ static inline char *timenow();
 
 //#define LOG_FMT             "%s | %-7s | %-15s | %s:%d | "
 //#define LOG_ARGS(LOG_TAG)   timenow(), LOG_TAG, _FILE, __FUNCTION__, __LINE__
-#define LOG_FMT             "%s | %s:%d | "
-#define LOG_ARGS(LOG_TAG)   _FILE, __FUNCTION__, __LINE__
+   
 
 #define NEWLINE     "\n"
 
