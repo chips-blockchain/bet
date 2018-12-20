d7b15128ac04ea7285e35f3a0aa688cdc948b3a3
chandram47 <chandram47@gmail.com>
2018-12-20 13:56:29 +0530

    debugging

--- privatebet/host.c
+++ privatebet/host.c
@@ -790,7 +790,7 @@ int32_t BET_check_BVV_Ready(struct privatebet_info *bet)
 	cJSON_AddItemToObject(bvvReady,"uri_info",uriInfo=cJSON_CreateArray());
 	for(int i=0;i<bet->maxplayers;i++)
 	{
-		printf("\n%s--->%s\n",dcv_info.uri[i],cJSON_Print(cJSON_CreateString(dcv_info.uri[i])))
+		printf("\n%s--->%s\n",dcv_info.uri[i],cJSON_Print(cJSON_CreateString(dcv_info.uri[i])));
 		cJSON_AddItemToArray(uriInfo,cJSON_CreateString(dcv_info.uri[i]));
 		cJSON_AddItemToArray(cJSON * array,cJSON * item)
 	}
