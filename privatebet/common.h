4fc1a58703f9aaf595c6c9000815d25d2cd88750
chandram47 <chandram47@gmail.com>
2018-12-21 12:04:11 +0530

    debugging

--- privatebet/common.h
+++ privatebet/common.h
@@ -24,15 +24,6 @@ extern char *LN_idstr,Host_ipaddr[64],Host_peerid[67],Host_channel[64];
 extern int32_t Num_hostrhashes,Chips_paid;
 extern bits256 playershares[CARDS777_MAXCARDS][CARDS777_MAXPLAYERS];
 
-#define hand_size 5
-#define NSUITS 4
-#define NFACES 13
-char* suit[NSUITS]= {"hearts","spades","clubs","diamonds"};
-char* face[NFACES]= {"ace","two","three","four","five","six","seven","eight","nine",
-                     "ten","jack","queen","king"
-                    };
-
-
 #endif
 
 
