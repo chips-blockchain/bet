92f1620d85c0480b52b249148ac824ae08370cf6
sg777 <sarath.ginjupalli89@gmail.com>
2019-04-09 12:06:04 +0530

    debugging

--- privatebet/client.c
+++ privatebet/client.c
@@ -2727,11 +2727,9 @@ int32_t BET_rest_player_turn(struct lws *wsi, cJSON *argjson)
 			printf("Failing to get own share: Decryption Error");
 			goto end;
 		}
-		printf("\nBET_player[this_playerID]->numplayers=%d\n",BET_player[this_playerID]->numplayers);
-		printf("\nBET_player[this_playerID]->range=%d\n",BET_player[this_playerID]->range);
 		for(int i=0;i<BET_player[this_playerID]->numplayers;i++)
 		{
-			if((!all_sharesflag[jint(argjson,"cardid")][i]) && (i != BET_player[this_playerID]->myplayerid))
+			if((!all_sharesflag[this_playerID][jint(argjson,"cardid")][i]) && (i != BET_player[this_playerID]->myplayerid))
 			{
 				retval=BET_rest_player_ask_share(wsi,jint(argjson,"cardid"),jint(argjson,"playerid"),jint(argjson,"card_type"));	
 			}
