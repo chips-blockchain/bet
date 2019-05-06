22666534a36f817bb709a40d8d3afc2b194019b1
sg777 <sarath.ginjupalli89@gmail.com>
2019-05-06 11:41:45 +0530

    debugging

--- privatebet/client.c
+++ privatebet/client.c
@@ -3148,6 +3148,70 @@ int32_t BET_rest_player_receive_share(struct lws *wsi,cJSON *argjson)
 }
 
 
+void rest_push_cards(struct lws *wsi,cJSON *argjson,int32_t this_playerID)
+{
+	
+	char* suit[NSUITS]= {"clubs","diamonds","hearts","spades"};
+	char* face[NFACES]= {"two","three","four","five","six","seven","eight","nine",
+						 "ten","jack","queen","king","ace"};
+	char* cards[52] = {"2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC", 
+					   "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AD", 
+					   "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH", 
+					   "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AS"};
+	
+	char action_str[8][100]={"","small_blind","big_blind","check","raise","call","allin","fold"};
+	cJSON *actions=NULL;
+	int flag;
+	cJSON *initCardInfo=NULL,*holeCardInfo=NULL,*initInfo=NULL,*boardCardInfo=NULL;
+
+	initInfo=cJSON_CreateObject();
+	cJSON_AddStringToObject(initInfo,"method","deal");
+	cJSON_AddItemToObject(initInfo,"deal",initCardInfo);
+	
+	initCardInfo=cJSON_CreateObject();
+	cJSON_AddNumberToObject(initCardInfo,"dealer",0);
+	
+	
+	holeCardInfo=cJSON_CreateArray();
+	cJSON_AddItemToObject(initCardInfo,"holecards",holeCardInfo);
+
+	for(int32_t i=0;((i<no_of_hole_cards)&&(i<all_number_cards_drawn[this_playerID]));i++)
+	{
+		cJSON_AddItemToArray(holeCardInfo,cJSON_CreateString(cards[all_player_card_values[this_playerID][i]]));
+	}
+
+	boardCardInfo=cJSON_CreateArray();
+	cJSON_AddItemToObject(initCardInfo,"boardcards",boardCardInfo);
+	
+	for(int32_t i=no_of_hole_cards;((i<hand_size)&&(i<all_number_cards_drawn[this_playerID]));i++)
+	{
+		cJSON_AddItemToArray(boardCardInfo,cJSON_CreateString(cards[all_player_card_values[this_playerID][i]]));
+	}
+	lws_write(wsi,cJSON_Print(initInfo),strlen(cJSON_Print(initInfo)),0);
+	/*		
+	printf("\n******************** Betting done so far ********************");
+	printf("\nsmall_blind:%d, big_blind:%d",small_blind_amount,big_blind_amount);
+	printf("\npot size:%d",jint(argjson,"pot"));
+	actions=cJSON_GetObjectItem(argjson,"actions");
+	int count=0;
+	flag=1;
+	for(int i=0;((i<=jint(argjson,"round"))&&(flag));i++)
+	{
+		printf("\nRound:%d",i);
+		for(int j=0;((j<BET_player[this_playerID]->maxplayers)&&(flag));j++)
+		{
+			if(jinti(actions,((i*BET_player[this_playerID]->maxplayers)+j))>0)
+				printf("\nplayed id:%d, action: %s",j,action_str[jinti(actions,((i*BET_player[this_playerID]->maxplayers)+j))]);
+			count++;	
+			if(count==cJSON_GetArraySize(actions))
+					flag=0;
+		}
+		printf("\n");
+	}
+	*/
+}
+
+
 void rest_display_cards(cJSON *argjson,int32_t this_playerID)
 {
 	
