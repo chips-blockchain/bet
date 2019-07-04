5cfe8130b9d547fefe0faf91c1641e953ac30f35
sg777 <sarath.ginjupalli89@gmail.com>
2019-07-04 21:12:04 +0530

    debugging

--- privatebet/client.c
+++ privatebet/client.c
@@ -1639,14 +1639,57 @@ int32_t BET_p2p_bet_round(cJSON *argjson,struct privatebet_info *bet,struct priv
 
 void display_cards(cJSON *argjson,struct privatebet_info *bet,struct privatebet_vars *vars)
 {
+
+	char* suit[NSUITS]= {"clubs","diamonds","hearts","spades"};
+	char* face[NFACES]= {"two","three","four","five","six","seven","eight","nine",
+						 "ten","jack","queen","king","ace"};
+	char* cards[52] = {"2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC", 
+					   "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AD", 
+					   "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH", 
+					   "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AS"};
 	
+	char action_str[8][100]={"","small_blind","big_blind","check","raise","call","allin","fold"};
+	cJSON *actions=NULL;
+	int flag;
+	cJSON *initCardInfo=NULL,*holeCardInfo=NULL,*initInfo=NULL,*boardCardInfo=NULL;
+
+	initInfo=cJSON_CreateObject();
+	cJSON_AddStringToObject(initInfo,"method","deal");
+	
+	initCardInfo=cJSON_CreateObject();
+	cJSON_AddNumberToObject(initCardInfo,"dealer",0);
+	
+	
+	holeCardInfo=cJSON_CreateArray();
+	for(int32_t i=0;((i<no_of_hole_cards)&&(i<number_cards_drawn));i++)
+	{
+		cJSON_AddItemToArray(holeCardInfo,cJSON_CreateString(cards[player_card_values[i]]));
+	}
+	
+	cJSON_AddItemToObject(initCardInfo,"holecards",holeCardInfo);
+
+	boardCardInfo=cJSON_CreateArray();
+	for(int32_t i=no_of_hole_cards;((i<hand_size)&&(i<number_cards_drawn));i++)
+	{
+		cJSON_AddItemToArray(boardCardInfo,cJSON_CreateString(cards[player_card_values[i]]));
+	}
+	
+	cJSON_AddItemToObject(initCardInfo,"board",boardCardInfo);
+
+	
+	cJSON_AddItemToObject(initInfo,"deal",initCardInfo);
+	printf("\n%s::%d::%s",__FUNCTION__,__LINE__,cJSON_Print(initInfo));
+	lws_write(wsi_global_client,cJSON_Print(initInfo),strlen(cJSON_Print(initInfo)),0);
+
+
+	#if 0	
 	char* suit[NSUITS]= {"clubs","diamonds","hearts","spades"};
 	char* face[NFACES]= {"two","three","four","five","six","seven","eight","nine",
 						 "ten","jack","queen","king","ace"};
 	char action_str[8][100]={"","small_blind","big_blind","check","raise","call","allin","fold"};
 	cJSON *actions=NULL;
 	int flag;
-
+	
 	printf("\n******************** Player Cards ********************");
 	printf("\nHole Cards:\n");
 	for(int32_t i=0;((i<no_of_hole_cards)&&(i<number_cards_drawn));i++)
@@ -1685,6 +1728,7 @@ void display_cards(cJSON *argjson,struct privatebet_info *bet,struct privatebet_
 		}
 		printf("\n");
 	}
+	#endif
 }
 int32_t BET_p2p_client_receive_share(cJSON *argjson,struct privatebet_info *bet,struct privatebet_vars *vars)
 {
@@ -3792,7 +3836,7 @@ void rest_push_cards(struct lws *wsi,cJSON *argjson,int32_t this_playerID)
 	
 	cJSON_AddItemToObject(initInfo,"deal",initCardInfo);
 	printf("\n%s::%d::%s",__FUNCTION__,__LINE__,cJSON_Print(initInfo));
-	lws_write(wsi_global_client,cJSON_Print(initInfo),strlen(cJSON_Print(initInfo)),0);
+	lws_write(wsi,cJSON_Print(initInfo),strlen(cJSON_Print(initInfo)),0);
 	/*		
 	printf("\n******************** Betting done so far ********************");
 	printf("\nsmall_blind:%d, big_blind:%d",small_blind_amount,big_blind_amount);
