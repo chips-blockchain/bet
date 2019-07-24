7b7abeef643d10ee9969b85449b6092165671c6b
sg777 <sarath.ginjupalli89@gmail.com>
2019-07-24 13:46:09 +0530

    debugging

--- privatebet/host.c
+++ privatebet/host.c
@@ -661,6 +661,26 @@ void BET_rest_DCV_reset(struct lws *wsi)
 	hole_cards_drawn=0;community_cards_drawn=0;flop_cards_drawn=0;turn_card_drawn=0;river_card_drawn=0;
 	invoiceID=0;	
 
+	
+	for(int i=0;i<BET_dcv->maxplayers;i++)
+			player_ready[i]=0;	
+
+	for(int k=0;k<BET_dcv->maxplayers;k++)
+	{
+		
+		for(int i=0;i<hand_size;i++)
+		{
+			for(int j=0;j<BET_dcv->maxplayers;j++)
+			{
+				all_player_card_matrix[k][j][i]=0;
+				all_player_card_values[k][j][i]=-1;
+				//card_matrix[j][i]=0;
+				//card_values[j][i]=-1;
+			}
+		}
+				
+	}
+		
 	BET_rest_dcv_init(wsi,NULL);
 }
 
