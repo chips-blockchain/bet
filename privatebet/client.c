8ee52716fd521e186e65933680e991ed988c5246
sg777 <sarath.ginjupalli89@gmail.com>
2019-08-08 16:36:58 +0530

    debugging

--- privatebet/client.c
+++ privatebet/client.c
@@ -2425,6 +2425,7 @@ int32_t BET_p2p_client_join(cJSON *argjson,struct privatebet_info *bet,struct pr
 		address=cJSON_GetArrayItem(addresses,0);
 		strcat(uri,jstr(address,"address"));
 		cJSON_AddStringToObject(joininfo,"uri",uri);
+		cJSON_AddNumberToObject(joininfo,"gui_playerID",jint(argjson,"gui_playerID"));
 
 		rendered=cJSON_Print(joininfo);
         bytes=nn_send(bet->pushsock,rendered,strlen(rendered),0);
