1bd644e45759f4bbae36d199aa7c0896cd88524a
sg777 <sarath.ginjupalli89@gmail.com>
2019-09-13 20:38:35 +0530

    debugging

--- privatebet/client.c
+++ privatebet/client.c
@@ -1166,7 +1166,7 @@ int32_t BET_p2p_bvvcommand(cJSON *argjson,struct privatebet_info *bet,struct pri
 	
     if ( (method= jstr(argjson,"method")) != 0 )
     {
-    	printf("%s::%d::method::%s\n",__FUNCTION__,__LINE__,method);
+    	//printf("%s::%d::method::%s\n",__FUNCTION__,__LINE__,method);
 		if ( strcmp(method,"TableInfo") == 0 )
 		{
 			BET_p2p_table_info(argjson,bet,vars);
@@ -2177,7 +2177,7 @@ int32_t BET_p2p_clientupdate(cJSON *argjson,struct privatebet_info *bet,struct p
 	//printf("%s::%d::%s\n",__FUNCTION__,__LINE__,cJSON_Print(argjson));
     if ( (method= jstr(argjson,"method")) != 0 )
     {
-       	printf("%s::%d::method::%s\n",__FUNCTION__,__LINE__,method);  
+       //	printf("%s::%d::method::%s\n",__FUNCTION__,__LINE__,method);  
     	if ( strcmp(method,"join") == 0 )
 		{
 			retval=BET_p2p_client_join(argjson,bet,vars);
