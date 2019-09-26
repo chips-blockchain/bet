35daf369ad85c9d073db71c233a3eb6ae88b7611
sg777 <sarath.ginjupalli89@gmail.com>
2019-09-26 12:02:01 +0530

    debugging

--- privatebet/bet.h
+++ privatebet/bet.h
@@ -397,17 +397,6 @@ struct deck_bvv_info
 	uint32_t numplayers,maxplayers;
 };
 
-
-char multisigaddresses[2][64]={"baf78zg1FEJo4AactZMD16iwmbEAZQVNL3"};
-
-char claimrelationship[5][100]={
-	"baf78zg1FEJo4AactZMD16iwmbEAZQVNL3:159.69.23.30",
-	"baf78zg1FEJo4AactZMD16iwmbEAZQVNL3:159.69.23.31",
-	"baf78zg1FEJo4AactZMD16iwmbEAZQVNL3:159.69.23.28",
-	"baf78zg1FEJo4AactZMD16iwmbEAZQVNL3:159.69.23.29"
-	};
-
-
 extern struct privatebet_info *BET_dcv_global;
 extern struct privatebet_info *BET_bvv_global;
 extern struct privatebet_info *BET_player_global;
