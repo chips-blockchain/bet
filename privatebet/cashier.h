3277dc5109e327c8832d36a665117f7a9ab11716
sg777 <sarath.ginjupalli89@gmail.com>
2020-01-02 11:08:50 +0100

    Adjusted to .clang-format style

--- privatebet/cashier.h
+++ privatebet/cashier.h
@@ -1,4 +1,4 @@
-char* bet_check_notary_status();
+char *bet_check_notary_status();
 int32_t bet_send_status(struct cashier *cashier_info);
 int32_t bet_cashier_backend(cJSON *argjson, struct cashier *cashier_info);
 void bet_cashier_server_loop(void *_ptr);
--- privatebet/network.h
+++ privatebet/network.h
@@ -1,4 +1,4 @@
 #include "bet.h"
-char* bet_get_etho_ip();
+char *bet_get_etho_ip();
 char *bet_tcp_sock_address(int32_t bindflag, char *str, char *ipaddr, uint16_t port);
 int32_t bet_nanosock(int32_t bindflag, char *endpoint, int32_t nntype);
