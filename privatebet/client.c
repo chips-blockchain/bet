20acd554f259838513a927bfd96991e34c57dc43
sg777 <sarath.ginjupalli89@gmail.com>
2020-01-26 11:13:22 +0530

    removing unused params in the display_cards function definition

--- privatebet/client.c
+++ privatebet/client.c
@@ -706,7 +706,7 @@ end:
 	return retval;
 }
 
-void display_cards(cJSON *argjson, struct privatebet_info *bet, struct privatebet_vars *vars)
+void display_cards()
 {
 	char *cards[52] = { "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC",
 			    "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AD",
