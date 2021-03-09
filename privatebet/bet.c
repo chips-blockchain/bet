57514821be263a4b74365cc06ca6dd8f0ae7c8a0
sg777 <sarath.ginjupalli89@gmail.com>
2021-03-09 02:06:29 +0100

    Enabling the heartbeat protocol

--- privatebet/bet.c
+++ privatebet/bet.c
@@ -263,11 +263,11 @@ static void bet_dcv_deinitialize()
 
 static void bet_dcv_thrd(char *dcv_ip, const int32_t port)
 {
-	pthread_t /*live_thrd,*/ dcv_backend, dcv_thrd;
+	pthread_t live_thrd, dcv_backend, dcv_thrd;
 
 	bet_dcv_initialize(dcv_ip, port);
 
-#if 0
+#if 1
 	if (OS_thread_create(&live_thrd, NULL, (void *)bet_dcv_heartbeat_loop, (void *)bet_dcv) != 0) {
 		printf("error launching bet_dcv_heartbeat_loop\n");
 		exit(-1);
@@ -287,7 +287,7 @@ static void bet_dcv_thrd(char *dcv_ip, const int32_t port)
 	if (pthread_join(dcv_thrd, NULL)) {
 		printf("\nError in joining the main thread for dcv_thrd");
 	}
-#if 0
+#if 1
 	if (pthread_join(live_thrd, NULL)) {
 		printf("\nError in joining the main thread for bet_dcv_heartbeat_loop");
 	}
