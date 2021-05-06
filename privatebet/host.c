7c29ffa97721095dd51a9486c38fe8548b0c2d29
sg777 <sarath.ginjupalli89@gmail.com>
2021-05-06 17:38:05 +0530

    Multiplayer WIP

--- privatebet/host.c
+++ privatebet/host.c
@@ -331,6 +331,7 @@ int32_t bet_dcv_deck_init_info(cJSON *argjson, struct privatebet_info *bet, stru
 			cJSON_AddItemToArray(cjson_dcv_blind_cards,
 					     cJSON_CreateString(bits256_str(str, dcv_info.dcvblindcards[i][j])));
 		}
+		printf("\n");
 	}
 
 	cJSON_AddItemToObject(deck_init_info, "g_hash", cjsong_hash = cJSON_CreateArray());
@@ -345,7 +346,7 @@ int32_t bet_dcv_deck_init_info(cJSON *argjson, struct privatebet_info *bet, stru
 		cJSON_AddItemToArray(cjson_peer_pub_keys,
 				     cJSON_CreateString(bits256_str(str, dcv_info.peerpubkeys[i])));
 	}
-	printf("%s::%d::dcvblindcards::%s\n", __FUNCTION__, __LINE__, cJSON_Print(cjson_dcv_blind_cards));
+	//printf("%s::%d::dcvblindcards::%s\n", __FUNCTION__, __LINE__, cJSON_Print(cjson_dcv_blind_cards));
 	rendered = cJSON_Print(deck_init_info);
 	bytes = nn_send(bet->pubsock, rendered, strlen(rendered), 0);
 
