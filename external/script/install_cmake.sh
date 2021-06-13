3d83bbcab07a1d47a58efd4928d099bd92f5d9fc
satindergrewal <grewal@lavabit.com>
2021-06-14 06:13:13 +1200

    moved install_cmake.sh to tools dir

--- external/script/install_cmake.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo mkdir /opt/cmake
-wget https://cmake.org/files/v3.16/cmake-3.16.1-Linux-x86_64.sh
-mv cmake-3.16.1-Linux-x86_64.sh /opt/cmake/
-sh /opt/cmake/cmake-3.16.1-Linux-x86_64.sh --prefix=/opt/cmake --skip-license
-sudo ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake
