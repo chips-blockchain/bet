f4f6724e706c085c93bd906cc5656fbaf2b2acff
satindergrewal <grewal@lavabit.com>
2021-06-14 06:55:13 +1200

    mkdir -p

--- tools/install_cmake.sh
+++ tools/install_cmake.sh
@@ -2,7 +2,7 @@
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     if ! command -v cmake &> /dev/null
     then
-        sudo mkdir /opt/cmake
+        sudo mkdir -p /opt/cmake
         wget https://cmake.org/files/v3.16/cmake-3.16.1-Linux-x86_64.sh
         mv cmake-3.16.1-Linux-x86_64.sh /opt/cmake/
         sh /opt/cmake/cmake-3.16.1-Linux-x86_64.sh --prefix=/opt/cmake --skip-license
