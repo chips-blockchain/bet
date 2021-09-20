4b60de8e73eaa5ed7cbb7e63f036f1d0b0759b3e
satindergrewal <grewal@lavabit.com>
2021-09-21 02:14:28 +1200

    CI/CD builds for linux x86_64/aarch64 and MacOS x86_64

--- tools/build_armv7l.sh
+++ tools/build_armv7l.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 docker build -t bet_armv7l:1.0 -f tools/Dockerfile_armv7l . && docker run -it --mount type=bind,source="$(pwd)",target=/root/Download bet_armv7l:1.0
 mkdir -p dist
 mv bet-linux-armv7l-$(printf '%s' $(git describe --always)).tar.gz dist/
--- tools/install_cmake.sh
+++ tools/install_cmake.sh
@@ -1,19 +1,45 @@
 #!/bin/bash
+
+INSTALL_CMAKE() {
+    echo "Install Cmake"
+    if [ ! -f external/cmake/bin/cmake ];
+    then
+        echo 'cmake does not exists... compiling...'
+        cd external/cmake && \
+        ./bootstrap && \
+        make
+        # sudo make install
+    else
+        echo 'cmake exists'
+        ls -lh external/cmake/bin/cmake
+        external/cmake/bin/cmake --version
+    fi
+}
+
 ARCH=$(uname -m)
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     if [[ $ARCH == "aarch64"* ]] || [[ $ARCH == "armv7l"* ]]; then
         echo '$ARCH equals'
-        if [ ! -f external/cmake/bin/cmake ];
+
+        if ! command -v cmake &> /dev/null
         then
-            echo 'cmake does not exists... compileing...'
-            cd external/cmake && \
-            ./bootstrap && \
-            make
-            # sudo make install
+            echo "cmake could not be found"
+            INSTALL_CMAKE
         else
-            echo 'cmake exists'
-            ls -lh external/cmake/bin/cmake
-            external/cmake/bin/cmake --version
+            echo "cmake is found"
+            CMAKE_VERSION=$(cmake --version | grep "cmake version" | cut -d " " -f 3)
+            # echo $CMAKE_VERSION
+
+            CMAKE_MAJOR_VERSION=$(echo $CMAKE_VERSION | cut -d "." -f 1)
+            CMAKE_MINOR_VERSION=$(echo $CMAKE_VERSION | cut -d "." -f 2)
+            CMAKE_VERSION_TO_COMPARE="$CMAKE_MAJOR_VERSION.$CMAKE_MINOR_VERSION"
+            CMAKE_MIN_VERSION_REQUIRED=3.13
+            if (( $(echo "$CMAKE_MIN_VERSION_REQUIRED > $CMAKE_VERSION_TO_COMPARE" |bc -l) )); then
+                echo "$CMAKE_VERSION_TO_COMPARE = You have lower version of cmake than required."
+                INSTALL_CMAKE
+            else
+                echo "$CMAKE_VERSION_TO_COMPARE = You have higher version of cmake than required."
+            fi
         fi
     else
         if ! command -v cmake &> /dev/null
