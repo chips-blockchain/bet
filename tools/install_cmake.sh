f493b70cbb298d99175cc9a115d3f517a22ebee3
satindergrewal <grewal@lavabit.com>
2021-10-05 18:56:23 +1300

    fix cmake install permissions for x86_64

--- tools/install_cmake.sh
+++ tools/install_cmake.sh
@@ -46,8 +46,8 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         then
             sudo mkdir -p /opt/cmake
             wget https://cmake.org/files/v3.16/cmake-3.16.1-Linux-x86_64.sh
-            mv cmake-3.16.1-Linux-x86_64.sh /opt/cmake/
-            sh /opt/cmake/cmake-3.16.1-Linux-x86_64.sh --prefix=/opt/cmake --skip-license
+            sudo mv cmake-3.16.1-Linux-x86_64.sh /opt/cmake/
+            sudo sh /opt/cmake/cmake-3.16.1-Linux-x86_64.sh --prefix=/opt/cmake --skip-license
             sudo ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake
         fi
     fi
