cbada1bcb79559b5b81c6e31ebf0f9d429c2c836
sg777 <sarath.ginjupalli89@gmail.com>
2021-11-02 21:48:03 +0530

    Adding abstraction over nn_send

--- privatebet/network.h
+++ privatebet/network.h
@@ -3,3 +3,5 @@ char *bet_get_etho_ip();
 char *bet_tcp_sock_address(int32_t bindflag, char *str, char *ipaddr, uint16_t port);
 int32_t bet_nanosock(int32_t bindflag, char *endpoint, int32_t nntype);
 cJSON *bet_msg_dealer_with_response_id(cJSON *argjson, char *dealer_ip, char *message);
+int32_t bet_send_data(int32_t socket, char *data, int32_t length);
+
