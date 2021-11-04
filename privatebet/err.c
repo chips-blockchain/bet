b0837e32d9067d8a1bb5c8f280edbf6e08793d4b
sg777 <sarath.ginjupalli89@gmail.com>
2021-11-04 05:56:54 +0530

    Adding error strings to the new error types that got defined

--- privatebet/err.c
+++ privatebet/err.c
@@ -18,6 +18,18 @@ const char *bet_err_str(int32_t err_no)
 	case ERR_PT_PLAYER_UNAUTHORIZED:
 		return "Player is unauthorized to join the private table hosting by the dealer";
 		break;
+	case ERR_NOT_ENOUGH_PLAYERS:
+		return "There should be atleast two players needed to play the game";
+		break;
+	case ERR_ALL_CARDS_DRAWN:
+		return "All cards are drawn from the deck";
+		break;
+	case ERR_DCV_COMMISSION_MISMATCH:
+		return "Dealer commission is not within the limits set by the player";
+		break;
+	case ERR_DEALER_TABLE_FULL:
+		return "Dealer table is FULL";
+		break;
 	case ERR_CHIPS_TX_SIGNING:
 		return "Error in signing the CHIPS tx";
 		break;
@@ -27,11 +39,29 @@ const char *bet_err_str(int32_t err_no)
 	case ERR_LN:
 		return "An LN Error in performing the LN operation";
 		break;
+	case ERR_LN_CHANNEL_ESTABLISHMENT:
+		return "Error in establishing LN channel";
+		break;
 	case ERR_LN_IS_NOT_OVER_TOR:
 		return "The LN channel is not established over TOR";
 		break;
-	case ERR_LN_CHANNEL_ESTABLISHMENT:
-		return "Error in establishing LN channel";
+	case ERR_CHIPS_INSUFFICIENT_FUNDS:
+		return "CHIPS Wallet doesn't have sufficient funds";
+		break;
+	case ERR_LN_INSUFFICIENT_FUNDS:
+		return "LN Wallet doesn't have sufficient funds";
+		break;
+	case ERR_CHIPS_TX_FAILED:
+		return "Error in making CHIPS TX";
+		break;
+	case ERR_LN_CHANNEL_FUNDING_FAILED:
+		return "Funding LN channel got failed";
+		break;
+	case ERR_LN_PAY:
+		return "Paying LN invoice got failed";
+		break;
+	case ERR_LN_ADDRESS_TYPE_MISMATCH:
+		return "LN address type is not matched with the one configured";
 		break;
 	case ERR_INI_PARSING:
 		return "Error in parsing the ini configuration file";
