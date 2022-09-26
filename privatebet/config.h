767097feba383a32568dd48630b3a4b098f573b7
sg777 <sarath.ginjupalli89@gmail.com>
2022-09-26 10:42:41 +0530

    We use .ini configuration files in bet, so removing .json configuration files which we used earlier to avoid ambiguity

--- privatebet/config.h
+++ privatebet/config.h
@@ -1,8 +1,5 @@
 #include "bet.h"
 cJSON *bet_read_json_file(char *file_name);
-void bet_parse_dealer_config_file();
-void bet_parse_cashier_nodes_file();
-void bet_parse_player_config_file();
 void bet_parse_dealer_config_ini_file();
 void bet_parse_player_config_ini_file();
 void bet_parse_cashier_config_ini_file();
