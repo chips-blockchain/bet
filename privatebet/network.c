940e56fb26eceee24f1ef2f32c9fbd7ea4931db5
sg777 <sarath.ginjupalli89@gmail.com>
2022-10-07 17:38:20 +0530

    debugging

--- privatebet/network.c
+++ privatebet/network.c
@@ -105,11 +105,11 @@ cJSON *bet_msg_dealer_with_response_id(cJSON *argjson, char *dealer_ip, char *me
 	bet_tcp_sock_address(0, bind_push_addr, dealer_ip, dealer_bvv_push_pull_port);
 	c_pushsock = bet_nanosock(0, bind_push_addr, NN_PUSH);
 
-	dlg_info("c_pushsock::%d, c_subsock ::%d", c_pushsock, c_subsock);
+	dlg_info("c_pushsock::%d, c_subsock ::%d, data::%s", c_pushsock, c_subsock, cJSON_Print(argjson));
 	
 	bytes = nn_send(c_pushsock, cJSON_Print(argjson), strlen(cJSON_Print(argjson)), 0);
 	if (bytes < 0) {
-		dlg_error("Failed to send data to the dealer :: %s", dealer_ip);
+		dlg_error("Failed to send data to the dealer :: %s, nn_send return ::%d", dealer_ip, bytes);
 		return NULL;
 	} else {
 		while (c_pushsock >= 0 && c_subsock >= 0) {
