eba3fa2533a9c392be61f6f23a503b0a426fae6e
sg777 <sarath.ginjupalli89@gmail.com>
2022-12-02 10:59:15 +0100

    wip

--- privatebet/vdxf.h
+++ privatebet/vdxf.h
@@ -7,14 +7,13 @@
 
 //Datatypes used
 #define iK7a5JNJnbeuYWVHCDRpJosj3irGJ5Qa8c "vrsc::data.type.string"
-#define STRING_VDXF_ID "iK7a5JNJnbeuYWVHCDRpJosj3irGJ5Qa8c" 
+#define STRING_VDXF_ID "iK7a5JNJnbeuYWVHCDRpJosj3irGJ5Qa8c"
 
 //Identitites
 #define CASHIERS_ID "cashiers.cashiers.chips10sec@"
 
 #define CASHIERS_CASHIERS_VDXF_ID "i6CS9ewyp4oWozG2eceXPk3uSHg3dihdPg"
 
-
-cJSON* get_cashiers();
-cJSON* get_onchain_cashiers();
+cJSON *get_cashiers();
+cJSON *get_onchain_cashiers();
 void update_cashiers();
