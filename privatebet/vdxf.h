5182d62cb7b9d52c434248aed50df71c969bb12c
sg777 <sarath.ginjupalli89@gmail.com>
2022-12-02 12:07:51 +0700

    vdxf WIP

---
+++ privatebet/vdxf.h
@@ -0,0 +1,16 @@
+#include "bet.h"
+#include "common.h"
+
+//Keys under a given namespace
+#define iJ3WZocnjG9ufv7GKUA4LijQno5gTMb7tP "chips.vrsc::bet.cashiers"
+
+//Datatypes used
+#define iK7a5JNJnbeuYWVHCDRpJosj3irGJ5Qa8c "vrsc::data.type.string"
+
+
+//Identitites
+#define CASHIERS_ID "cashiers.cashiers.chips10sec@"
+
+
+cJSON* get_cashiers();
+void update_cashiers();
