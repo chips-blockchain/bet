33f3c0c2fde0a3894eecff2e53f65bdbddb82ccf
sg777 <sarath.ginjupalli89@gmail.com>
2022-12-06 17:01:40 +0700

    wip

--- privatebet/misc.h
+++ privatebet/misc.h
@@ -3,5 +3,7 @@ int32_t hexstr_to_str(char *input, char *output);
 void str_to_hexstr(char *input, char *output);
 void delete_file(char *file_name);
 int check_url(const char *url);
-void float_to_uint32(int *s, int *m, int *e, float number);
+void float_to_uint32(uint32_t *s, uint32_t *m, uint32_t *e, float number);
 void float_to_uint32_s(struct float_num *t, float number);
+void uint32_s_to_float(struct float_num t, float *number);
+uint8_t* struct_to_byte_arr(const void *object, size_t size);
