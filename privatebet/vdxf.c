830b5cd4a1ba1d538851e112422e36edb4afabc3
sg777 <sarath.ginjupalli89@gmail.com>
2022-12-07 13:09:55 +0700

    wip

--- privatebet/vdxf.c
+++ privatebet/vdxf.c
@@ -2,109 +2,6 @@
 #include "commands.h"
 #include "misc.h"
 
-cJSON *get_cashiers()
-{
-	int argc = 3;
-	char **argv = NULL;
-	cJSON *argjson = NULL, *contentmultimap = NULL, *cashiers = NULL, *cashier_ips = NULL;
-
-	bet_alloc_args(argc, &argv);
-	strncpy(blockchain_cli, verus_chips_cli, strlen(verus_chips_cli));
-	argv = bet_copy_args(argc, verus_chips_cli, "getidentity", CASHIERS_ID);
-	argjson = cJSON_CreateObject();
-	make_command(argc, argv, &argjson);
-	contentmultimap = cJSON_GetObjectItem(cJSON_GetObjectItem(argjson, "identity"), "contentmultimap");
-
-	cashiers = cJSON_CreateArray();
-	cashiers = cJSON_GetObjectItem(contentmultimap, CASHIERS_KEY);
-
-	cashier_ips = cJSON_CreateArray();
-	for (int32_t i = 0; i < cJSON_GetArraySize(cashiers); i++) {
-		cJSON_AddItemToArray(cashier_ips, cJSON_GetObjectItem(cJSON_GetArrayItem(cashiers, i), STRING_VDXF_ID));
-	}
-	dlg_info("%s::%d::cashier ips::%s\n", __FUNCTION__, __LINE__, cJSON_Print(cashier_ips));
-
-end:
-	bet_dealloc_args(argc, &argv);
-}
-
-cJSON *get_onchain_cashiers()
-{
-	int argc = 3;
-	char **argv = NULL;
-	cJSON *argjson = NULL, *contentmultimap = NULL, *cashiers = NULL;
-
-	bet_alloc_args(argc, &argv);
-	strncpy(blockchain_cli, verus_chips_cli, strlen(verus_chips_cli));
-	argv = bet_copy_args(argc, verus_chips_cli, "getidentity", CASHIERS_ID);
-	argjson = cJSON_CreateObject();
-	make_command(argc, argv, &argjson);
-	contentmultimap = cJSON_GetObjectItem(cJSON_GetObjectItem(argjson, "identity"), "contentmultimap");
-
-	dlg_info("\ncontentmultimap :: %s\n", cJSON_Print(contentmultimap));
-
-	cashiers = cJSON_CreateArray();
-	cashiers = cJSON_GetObjectItem(contentmultimap, CASHIERS_KEY);
-
-	dlg_info("\ncashiers :: %s\n", cJSON_Print(cashiers));
-
-	for (int32_t i = 0; i < cJSON_GetArraySize(cashiers); i++) {
-		dlg_info("IP :: %s\n",
-			 cJSON_Print(cJSON_GetObjectItem(cJSON_GetArrayItem(cashiers, i), STRING_VDXF_ID)));
-	}
-
-end:
-	bet_dealloc_args(argc, &argv);
-
-	return cashiers;
-}
-
-void update_cashiers(char *ip)
-{
-	cJSON *cashiers_info = NULL, *update_info = NULL, *ip_obj = NULL, *argjson = NULL, *cashier_ips = NULL;
-	int argc;
-	char **argv = NULL;
-	char params[arg_size] = { 0 };
-
-	cashiers_info = cJSON_CreateObject();
-	cashier_ips = get_onchain_cashiers();
-
-	ip_obj = cJSON_CreateObject();
-	cJSON_AddStringToObject(ip_obj, STRING_VDXF_ID, ip);
-
-	if (NULL == cashier_ips)
-		cashier_ips = cJSON_CreateArray();
-	else {
-		for (int i = 0; i < cJSON_GetArraySize(cashier_ips); i++) {
-			if (0 == strcmp(jstr(cJSON_GetArrayItem(cashier_ips, i), STRING_VDXF_ID), ip)) {
-				dlg_info("%s::%d::The latest data of this ID contains this %s\n", __FUNCTION__,
-					 __LINE__, ip);
-				goto end;
-			}
-		}
-	}
-
-	cJSON_AddItemToArray(cashier_ips, ip_obj);
-	cJSON_AddItemToObject(cashiers_info, CASHIERS_KEY, cashier_ips);
-
-	update_info = cJSON_CreateObject();
-	cJSON_AddStringToObject(update_info, "name", "cashiers");
-	cJSON_AddStringToObject(update_info, "parent", POKER_CHIPS_VDXF_ID);
-	cJSON_AddItemToObject(update_info, "contentmultimap", cashiers_info);
-
-	argc = 3;
-	bet_alloc_args(argc, &argv);
-	snprintf(params, arg_size, "\'%s\'", cJSON_Print(update_info));
-	argv = bet_copy_args(argc, verus_chips_cli, "updateidentity", params);
-
-	argjson = cJSON_CreateObject();
-	make_command(argc, argv, &argjson);
-	dlg_info("\n%s::%d::%s\n", __FUNCTION__, __LINE__, cJSON_Print(argjson));
-
-end:
-	bet_dealloc_args(argc, &argv);
-}
-
 cJSON *update_cmm(char *id, cJSON *cmm)
 {
 	cJSON *id_info = NULL, *argjson = NULL;
@@ -131,7 +28,6 @@ cJSON *update_cmm(char *id, cJSON *cmm)
 
 end:
 	bet_dealloc_args(argc, &argv);
-
 	dlg_info("%s::%d::%s\n", __FUNCTION__, __LINE__, cJSON_Print(argjson));
 	return argjson;
 }
@@ -227,4 +123,45 @@ struct table *get_dealers_config_table(char *dealer_id)
 
 end:
 	return t;
+}
+
+cJSON* get_cashiers_info(char *cashier_id)
+{
+	cJSON *cashier_cmm_data = NULL;
+	
+	cashier_cmm_data = cJSON_CreateObject();
+	cashier_cmm_data = get_cmm_key_data(cashier_id, 0, CASHIERS_KEY);
+
+end:
+	return cashier_cmm_data;
+}
+
+cJSON* update_cashiers(char *ip)
+{
+	cJSON *cashiers_info = NULL, *out = NULL, *ip_obj = NULL, *cashier_ips = NULL;
+
+	cashiers_info = cJSON_CreateObject();
+	cashier_ips = get_cashiers_info("cashiers");
+
+	ip_obj = cJSON_CreateObject();
+	cJSON_AddStringToObject(ip_obj, STRING_VDXF_ID, ip);
+
+	if (NULL == cashier_ips)
+		cashier_ips = cJSON_CreateArray();
+	else {
+		for (int i = 0; i < cJSON_GetArraySize(cashier_ips); i++) {
+			if (0 == strcmp(jstr(cJSON_GetArrayItem(cashier_ips, i), STRING_VDXF_ID), ip)) {
+				dlg_info("%s::%d::The latest data of this ID contains this %s\n", __FUNCTION__,
+					 __LINE__, ip);
+				goto end;
+			}
+		}
+	}
+
+	cJSON_AddItemToArray(cashier_ips, ip_obj);
+	cJSON_AddItemToObject(cashiers_info, CASHIERS_KEY, cashier_ips);
+	out = update_cmm("cashiers",cashiers_info);
+	
+end:
+	return out;
 }
--- privatebet/vdxf.h
+++ privatebet/vdxf.h
@@ -55,11 +55,11 @@ under chips as sg777.chips@ which basically been used to hold the tokens.
 #define CASHIERS_ID "cashiers.poker.chips10sec@"
 #define POKER_CHIPS_VDXF_ID "i6gViGxt7YinkJZoubKdbWBrqdRCb1Rkvs"
 
-cJSON *get_cashiers();
-cJSON *get_onchain_cashiers();
-void update_cashiers();
+
 cJSON *update_cmm(char *id, cJSON *cmm);
 cJSON *get_cmm(char *id, int16_t full_id);
 cJSON *get_cmm_key_data(char *id, int16_t full_id, char *key);
 cJSON *update_dealers_config_table(char *dealer_id, struct table t);
 struct table *get_dealers_config_table(char *dealer_id);
+cJSON* get_cashiers_info(char *cashier_id);
+cJSON* update_cashiers(char *ip);
