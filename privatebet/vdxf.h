63c6d76b60e7421fd4b99965081b1fb064390904
sg777 <sarath.ginjupalli89@gmail.com>
2022-12-08 13:21:17 +0700

    wip

--- privatebet/vdxf.h
+++ privatebet/vdxf.h
@@ -66,3 +66,6 @@ cJSON *get_dealers();
 struct table *find_table();
 bool is_id_exists(char *id, int16_t full_id);
 void verus_sendcurrency_data(cJSON *data);
+cJSON* getaddressutxos(char verus_addresses[][], int n);
+void test_loop();
+
