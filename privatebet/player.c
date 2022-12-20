4bec7c51f675baf92f428413a85cbd5d1607fb87
sg777 <sarath.ginjupalli89@gmail.com>
2022-12-20 16:35:59 +0700

    wip

--- privatebet/player.c
+++ privatebet/player.c
@@ -5,6 +5,54 @@
 #include "vdxf.h"
 #include "misc.h"
 
+cJSON *append_t_key(char *id, char *key, cJSON *key_info)
+{
+	int32_t argc, no_of_t_keys = 11;
+	char **argv = NULL, params[arg_size] = { 0 };
+	char all_t_keys[no_of_t_keys][128] = {T_TABLE_INFO_KEY, T_PLAYER_INFO_KEY, T_PLAYER1_KEY, T_PLAYER2_KEY, T_PLAYER3_KEY, T_PLAYER4_KEY, T_PLAYER5_KEY, T_PLAYER6_KEY, T_PLAYER7_KEY, T_PLAYER8_KEY, T_PLAYER9_KEY };
+	cJSON *id_info = NULL, *argjson = NULL, *cmm = NULL;
+		
+	if ((NULL == id) || (NULL == key) || (NULL == key_info) || (NULL == verus_chips_cli)) {
+		return NULL;
+	}
+
+	id_info = cJSON_CreateObject();
+	cJSON_AddStringToObject(id_info, "name", id);
+	cJSON_AddStringToObject(id_info, "parent", POKER_CHIPS_VDXF_ID);
+
+	cmm = cJSON_CreateObject();
+	cJSON_AddItemToObject(cmm, key, key_info);
+
+	for(int32_t i=0; i<no_of_t_keys; i++){
+		if(strcmp(all_t_keys[i],key) != 0) {
+			cJSON *temp = get_cmm_key_data(id,0,all_t_keys[i]);
+			if(temp){
+				cJSON_AddItemToObject(cmm, all_t_keys[i], temp);		
+			}			
+		}
+	}
+
+	cJSON_AddItemToObject(id_info, "contentmultimap", cmm);
+
+	argc = 3;
+	bet_alloc_args(argc, &argv);
+	snprintf(params, arg_size, "\'%s\'", cJSON_Print(id_info));
+	argv = bet_copy_args(argc, verus_chips_cli, "updateidentity", params);
+
+	//This is a temporary wait until we have an API to spend the tx's from mempool
+	while(!chips_is_mempool_empty()){
+		sleep(1);
+	}
+	
+	argjson = cJSON_CreateObject();
+	make_command(argc, argv, &argjson);
+
+end:
+	bet_dealloc_args(argc, &argv);
+	return argjson;
+}
+
+
 int32_t bet_init_player_deck(int32_t player_id)
 {
 	int32_t retval = OK;
@@ -38,11 +86,9 @@ int32_t bet_init_player_deck(int32_t player_id)
 	cmm = cJSON_CreateArray();
 	jaddi(cmm,player_deck_hex);
 	dlg_info("%s::%dcmm::%s\n", __func__, __LINE__, cJSON_Print(cmm));
+	
+	cJSON *out = append_t_key(player_config.table_id,T_PLAYER_KEYS[player_id-1], cmm);
 
-	cJSON *temp = NULL;
-	temp = cJSON_CreateObject();
-	cJSON_AddItemToObject(temp,T_PLAYER_KEYS[player_id-1], cmm);
-	cJSON *out = update_cmm(player_config.table_id, temp);
 	dlg_info("%s::%d::%s", __func__, __LINE__, cJSON_Print(out));
 
 end:
--- privatebet/player.h
+++ privatebet/player.h
@@ -1,2 +1,4 @@
 #include "bet.h"
+
+cJSON *append_t_key(char *id, char *key, cJSON *key_info);
 int32_t bet_init_player_deck(int32_t player_id);
