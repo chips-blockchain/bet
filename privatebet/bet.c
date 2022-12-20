20fe0640fbcd094e335c632f5a782342a4c61404
sg777 <sarath.ginjupalli89@gmail.com>
2022-12-20 10:41:57 +0700

    wip

--- privatebet/bet.c
+++ privatebet/bet.c
@@ -644,15 +644,15 @@ void test_crypto()
 
 int main(int argc, char **argv)
 {
-	test_crypto();
+	//test_crypto();
 
-#if 1 //Enable this snippet to make bet take no action on blocknotify
+#if 0 //Enable this snippet to make bet take no action on blocknotify
 	if ((argc == 3) && (strcmp(argv[1], "newblock") == 0)) {
 		goto end;
 	}
 #endif
 	//bet_init_player_deck(player_config.player_id);
-	//bet_start(argc, argv);
+	bet_start(argc, argv);
 
 end:
 	return OK;
