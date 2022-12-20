8c7274b68d46738b9d792be9bdce11d8d52fbb5d
sg777 <sarath.ginjupalli89@gmail.com>
2022-12-20 11:23:01 +0700

    wip

--- privatebet/player.c
+++ privatebet/player.c
@@ -36,7 +36,7 @@ int32_t bet_init_player_deck(int32_t player_id)
 	jaddstr(player_deck_hex,STRING_VDXF_ID,hexstr);
 	
 	cmm = cJSON_CreateObject();
-	jaddstr(cmm, T_PLAYER_KEYS[player_id-1], player_deck_hex);
+	jaddstr(cmm, T_PLAYER_KEYS[player_id-1], cJSON_Print(player_deck_hex));
 	dlg_info("%s::%d::%s\ncmm::%s", __func__, __LINE__, cJSON_Print(player_deck), cJSON_Print(cmm));
 
 	cJSON *temp = get_cmm(player_config.table_id,0);
