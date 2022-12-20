1cfe0f87bc7b6904f8e3e29f48a0dd5f042101a9
sg777 <sarath.ginjupalli89@gmail.com>
2022-12-20 04:33:50 +0100

    wip

--- privatebet/bet.c
+++ privatebet/bet.c
@@ -616,39 +616,37 @@ void test_crypto()
 	struct pair256 key;
 	bits256 r1, r2, r3;
 	bits256 p, d, b, r1_dup, d_pub;
-	bits256 p_dup,d_dup;
-	char hexstr [65];
-	
+	bits256 p_dup, d_dup;
+	char hexstr[65];
+
 	key.priv = curve25519_keypair(&key.prod);
-	r1 = card_rand256(1,10);
+	r1 = card_rand256(1, 10);
 	r2 = curve25519_keypair(&d_pub);
 	r3 = rand256(1);
 
-	p = fmul_donna(r1,key.prod);
-	d = fmul_donna(p,r2);
-	b= fmul_donna(d,r3);
-	
+	p = fmul_donna(r1, key.prod);
+	d = fmul_donna(p, r2);
+	b = fmul_donna(d, r3);
+
 	d_dup = fmul_donna(b, crecip_donna(r3));
-	p_dup = fmul_donna(d_dup, crecip_donna(r2)); 
-	r1_dup = fmul_donna(p_dup, crecip_donna(key.prod));  
-	
-	dlg_info("%s::%d::p::%s\n", __func__, __LINE__, bits256_str(hexstr,p));
-	dlg_info("%s::%d::d::%s\n", __func__, __LINE__, bits256_str(hexstr,d));
-	dlg_info("%s::%d::b::%s\n", __func__, __LINE__, bits256_str(hexstr,b));
-	
-	dlg_info("%s::%d::p_dup %s\n", __func__, __LINE__, bits256_str(hexstr,p_dup));
-	dlg_info("%s::%d::r1_dup::%s\n", __func__, __LINE__,bits256_str(hexstr,r1_dup));
+	p_dup = fmul_donna(d_dup, crecip_donna(r2));
+	r1_dup = fmul_donna(p_dup, crecip_donna(key.prod));
+
+	dlg_info("%s::%d::p::%s\n", __func__, __LINE__, bits256_str(hexstr, p));
+	dlg_info("%s::%d::d::%s\n", __func__, __LINE__, bits256_str(hexstr, d));
+	dlg_info("%s::%d::b::%s\n", __func__, __LINE__, bits256_str(hexstr, b));
+
+	dlg_info("%s::%d::p_dup %s\n", __func__, __LINE__, bits256_str(hexstr, p_dup));
+	dlg_info("%s::%d::r1_dup::%s\n", __func__, __LINE__, bits256_str(hexstr, r1_dup));
 
 	dlg_info("%s::%d::card r1 :: %d r1_dup::%d\n", __func__, __LINE__, r1.bytes[30], r1_dup.bytes[30]);
-			
 }
 
 int main(int argc, char **argv)
 {
+	test_crypto();
 
-test_crypto();
-
-#if 1 //Enable this snippet to make bet take no action on blocknotify	
+#if 1 //Enable this snippet to make bet take no action on blocknotify
 	if ((argc == 3) && (strcmp(argv[1], "newblock") == 0)) {
 		goto end;
 	}
--- privatebet/player.c
+++ privatebet/player.c
@@ -8,33 +8,33 @@ int32_t bet_init_player_deck(int32_t player_id)
 	int32_t retval = OK;
 	char str[65], *hexstr = NULL;
 	cJSON *cjson_player_cards = NULL, *player_deck = NULL, *cmm = NULL;
-	
 
-	if((player_id<1) && (player_id>9)){
+	if ((player_id < 1) && (player_id > 9)) {
 		retval = ERR_INVALID_PLAYER_ID;
 		goto end;
 	}
-	player_info.player_key = deckgen_player(player_info.cardprivkeys, player_info.cardpubkeys, player_info.permis, CARDS777_MAXCARDS);
+	player_info.player_key = deckgen_player(player_info.cardprivkeys, player_info.cardpubkeys, player_info.permis,
+						CARDS777_MAXCARDS);
 
 	player_deck = cJSON_CreateObject();
 	jaddnum(player_deck, "id", player_id);
-	jaddbits256(player_deck,"pubkey",player_info.player_key.prod);
-	jadd(player_deck,"cardinfo",cjson_player_cards = cJSON_CreateArray());
+	jaddbits256(player_deck, "pubkey", player_info.player_key.prod);
+	jadd(player_deck, "cardinfo", cjson_player_cards = cJSON_CreateArray());
 	for (int32_t i = 0; i < CARDS777_MAXCARDS; i++) {
-		jaddistr(cjson_player_cards,bits256_str(str, player_info.cardpubkeys[i]));
+		jaddistr(cjson_player_cards, bits256_str(str, player_info.cardpubkeys[i]));
 	}
 	cJSON_hex(player_deck, &hexstr);
-	if(hexstr == NULL){
+	if (hexstr == NULL) {
 		retval = ERR_PLAYER_DECK_SHUFFLING;
 		goto end;
 	}
 	cmm = cJSON_CreateObject();
-	jaddstr(cmm,T_PLAYER_KEYS[player_id],hexstr);
+	jaddstr(cmm, T_PLAYER_KEYS[player_id], hexstr);
 
-	dlg_info("%s::%d::%s\ncmm::%s", __func__, __LINE__, cJSON_Print(player_deck), cJSON_Print(cmm));	
-	cJSON * out = update_cmm(player_config.table_id,cmm);
+	dlg_info("%s::%d::%s\ncmm::%s", __func__, __LINE__, cJSON_Print(player_deck), cJSON_Print(cmm));
+	cJSON *out = update_cmm(player_config.table_id, cmm);
 	dlg_info("%s::%d::%s", __func__, __LINE__, cJSON_Print(out));
 
-	end:
-		return retval;
+end:
+	return retval;
 }
