779de7a82fcc0b5a2a4f984b22d054ec108161ea
sg777 <sarath.ginjupalli89@gmail.com>
2022-12-23 17:44:49 +0700

    wip

--- privatebet/deck.c
+++ privatebet/deck.c
@@ -30,7 +30,7 @@ void shuffle_deck(struct pair256 *r, int32_t n, int32_t *perm)
 	}
 }
 
-void shuffle_deck_db(struct bits256 *r, int32_t n, int32_t *perm)
+void shuffle_deck_db(bits256 *r, int32_t n, int32_t *perm)
 {
 	bits256 *tmp = NULL;
 	tmp = calloc(n, sizeof(bits256));
