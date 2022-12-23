8493d18ca450a7c6ba70a0620d01e498a8b4335d
sg777 <sarath.ginjupalli89@gmail.com>
2022-12-23 18:00:48 +0700

    wip

--- privatebet/deck.h
+++ privatebet/deck.h
@@ -3,3 +3,4 @@ struct pair256 gen_keypair();
 void gen_deck(struct pair256 *r, int32_t n);
 void shuffle_deck(struct pair256 *r, int32_t n, int32_t *perm);
 void shuffle_deck_db(bits256 *r, int32_t n, int32_t *perm);
+void blind_deck(bits256 *r, int32_t n, struct pair256 *blinder);
