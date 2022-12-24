9e5313bf0770b81a296df298e1c14fb8fb3d7fec
sg777 <sarath.ginjupalli89@gmail.com>
2022-12-24 05:13:33 +0100

    wip

--- privatebet/deck.c
+++ privatebet/deck.c
@@ -15,17 +15,17 @@ void gen_deck(struct pair256 *r, int32_t n)
 		tmp.priv = card_rand256(1, i);
 		tmp.prod = curve25519(tmp.priv, curve25519_basepoint9());
 		r[i] = tmp;
-	}	
+	}
 }
 
 void shuffle_deck(struct pair256 *r, int32_t n, int32_t *perm)
 {
 	struct pair256 *tmp = NULL;
 	tmp = calloc(n, sizeof(struct pair256));
-	for(int32_t i=0; i<n; i++){
+	for (int32_t i = 0; i < n; i++) {
 		tmp[i] = r[perm[i]];
 	}
-	for(int32_t i=0; i<n; i++){
+	for (int32_t i = 0; i < n; i++) {
 		r[i] = tmp[i];
 	}
 }
@@ -34,25 +34,24 @@ void shuffle_deck_db(bits256 *r, int32_t n, int32_t *perm)
 {
 	bits256 *tmp = NULL;
 	tmp = calloc(n, sizeof(bits256));
-	for(int32_t i=0; i<n; i++){
+	for (int32_t i = 0; i < n; i++) {
 		tmp[i] = r[perm[i]];
 	}
-	for(int32_t i=0; i<n; i++){
+	for (int32_t i = 0; i < n; i++) {
 		r[i] = tmp[i];
 	}
 }
 
 void blind_deck_d(bits256 *r, int32_t n, struct pair256 *blinder)
 {
-	for(int32_t i=0; i<n; i++){
-		r[i] = curve25519(blinder[i].priv,r[i]);
+	for (int32_t i = 0; i < n; i++) {
+		r[i] = curve25519(blinder[i].priv, r[i]);
 	}
 }
 
 void blind_deck_b(bits256 *r, int32_t n, struct pair256 *blinder)
 {
-	for(int32_t i=0; i<n; i++){
-		r[i] = fmul_donna(blinder[i].priv,r[i]);
+	for (int32_t i = 0; i < n; i++) {
+		r[i] = fmul_donna(blinder[i].priv, r[i]);
 	}
 }
-
