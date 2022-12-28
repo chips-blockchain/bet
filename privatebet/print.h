5c8f6dd107f6a84ccd019d76f7964f3588efde32
sg777 <sarath.ginjupalli89@gmail.com>
2022-12-28 13:29:21 +0700

    wip

--- privatebet/print.h
+++ privatebet/print.h
@@ -1,6 +1,8 @@
 #include "bet.h"
 
 void print_struct_table(struct table *t);
+void print_cashiers_id(char *id);
+void print_dealers_id(char *id);
 void print_dealer_id(char *id);
 void print_table_id(char *id);
 void print_id_info(int argc, char **argv);
