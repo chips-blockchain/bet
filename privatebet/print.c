4fc15741e2c37d83ca12a84f476fc69ba4726464
sg777 <sarath.ginjupalli89@gmail.com>
2022-12-28 13:31:09 +0700

    wip

--- privatebet/print.c
+++ privatebet/print.c
@@ -81,10 +81,14 @@ void print_id_info(int argc, char **argv)
 	if(argc == 4) {
 		if((strcmp(argv[3], "t") == 0) || (strcmp(argv[3], "table") == 0)){
 			print_table_id(argv[2]);
-		} else if((strcmp(argv[3], "d") || if(strcmp(argv[3], "dealer")) == 0){
+		} else if((strcmp(argv[3], "d") || (strcmp(argv[3], "dealer")) == 0){
 			print_dealer_id(argv[2]);
 		} else if(strcmp(argv[3], "dealers") == 0){
 			print_dealers_id(argv[2]);
+		} else if(strcmp(argv[3], "cashiers") == 0){
+			print_dealers_id(argv[2]);
+		} else {
+			dlg_info("Print is not supported for this ID::%s of type::%s", argv[2], argv[3]);
 		}
 	}
 }
