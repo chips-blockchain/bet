ff2b610398a1968dca0917e2b47298244a04d583
sg777 <sarath.ginjupalli89@gmail.com>
2022-12-30 05:57:50 +0700

    Fixing issue with game_id

--- privatebet/print.h
+++ privatebet/print.h
@@ -5,5 +5,6 @@ void print_cashiers_id(char *id);
 void print_dealers_id(char *id);
 void print_dealer_id(char *id);
 void print_table_id(char *id);
+void print_table_key_info(int argc, char **argv);
 void print_id_info(int argc, char **argv);
 void print_vdxf_info(int argc, char **argv);
