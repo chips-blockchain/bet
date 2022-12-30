b3c7b8e86f5e7b92f5f5a79c93b57d0dc7e04a9a
sg777 <sarath.ginjupalli89@gmail.com>
2022-12-30 09:26:25 +0700

    wip

--- privatebet/bet.c
+++ privatebet/bet.c
@@ -610,14 +610,14 @@ end:
 
 int main(int argc, char **argv)
 {
-	test_dealer_sb("sg777_t");
+	//test_dealer_sb("sg777_t");
 
 #if 1 //Enable this snippet to make bet take no action on blocknotify
 	if ((argc == 3) && (strcmp(argv[1], "newblock") == 0)) {
 		goto end;
 	}
 #endif
-	//bet_start(argc, argv);
+	bet_start(argc, argv);
 
 
 end:
