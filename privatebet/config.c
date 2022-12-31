882fc6ede39b3dd725c50615c836d86095efe6d4
sg777 <sarath.ginjupalli89@gmail.com>
2022-12-31 11:01:33 +0700

    wip

--- privatebet/config.c
+++ privatebet/config.c
@@ -313,28 +313,7 @@ int32_t bet_parse_verus_dealer()
 		strncpy(t.table_id, iniparser_getstring(ini, "table:table_id", NULL), sizeof(t.table_id));
 	}
 
-	//Updating the dealer id with t_table_info
-	out = update_cmm_from_id_key_data_cJSON(t.dealer_id, get_vdxf_id(T_TABLE_INFO_KEY), struct_table_to_cJSON(&t),
-						true);
-	dlg_info("%s", cJSON_Print(out));
-
-	retval = dealer_table_init(t);
-	if (retval)
-		dlg_error("%s", bet_err_str(retval));
-
-#if 0		
-	//Updating the table id with the game_id and t_table_info
-	cJSON *t2 =
-		append_cmm_from_id_key_data_hex(t.table_id, T_GAME_ID_KEY, bits256_str(hexstr, game_id), false);
-	dlg_info("%s", cJSON_Print(t2));
-
-	dlg_info("t_table_info key of the table id is::%s\n",
-		 get_key_data_vdxf_id(T_TABLE_INFO_KEY, bits256_str(hexstr, game_id)));
-	cJSON *t3 = append_cmm_from_id_key_data_cJSON(
-		t.table_id, get_key_data_vdxf_id(T_TABLE_INFO_KEY, bits256_str(hexstr, game_id)),
-		struct_table_to_cJSON(&t), true);
-	dlg_info("%s", cJSON_Print(t3));
-#endif
+	retval = dealer_init(t);
 	return retval;
 }
 
