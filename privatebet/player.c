1a2e1067ad3345be26aff99af3823ca2e60386b1
sg777 <sarath.ginjupalli89@gmail.com>
2023-01-02 19:48:37 +0700

    wip

--- privatebet/player.c
+++ privatebet/player.c
@@ -49,3 +49,109 @@ int32_t player_init_deck()
 
 	return retval;
 }
+
+
+void decode_card(bits256 b_blinded_card, bits256 blinded_value, cJSON *dealer_blind_info)
+{
+	int32_t retval = OK;
+	char str1[65], str2[65];
+	bits256 blinded_value_inv, d_blinded_card;
+	
+	
+	blinded_value_inv = crecip_donna(blinded_value);
+	d_blinded_card = fmul_donna(blinded_value_inv, b_blinded_card);
+
+		
+	for (int32_t i = 0; i < CARDS777_MAXCARDS; i++) {
+		for (int32_t j = 0; j < CARDS777_MAXCARDS; j++) {
+			if (strcmp(bits256_str(str1, d_blinded_card),
+				   bits256_str(str2, curve25519(p_deck_info.player_r[i].priv, jbits256i(dealer_blind_info,j)))) == 0) {
+				dlg_info("card::%x\n", p_deck_info.player_r[i].priv.bytes[30]);
+			}
+		}
+	}
+	dlg_info("No card found");
+}
+
+void reveal_card(char *table_id)
+{
+	int32_t retval = OK, player_id, card_id;
+	char *game_id_str = NULL;
+	cJSON *game_state_info = NULL, *bv_info = NULL, *b_blinded_deck = NULL,  *dealer_blind_info = NULL;
+	bits256 b_blinded_card, blinded_value;
+	
+	game_state_info = get_game_state_info(table_id);
+	player_id = jint(game_state_info, "player_id");
+	card_id = jint(game_state_info, "card_id");
+
+	if((player_id == player_config.player_id) || (player_id == -1)){
+		game_id_str = get_cJSON_from_id_key(table_id, T_GAME_ID_KEY);
+		bv_info = get_cJSON_from_id_key_vdxfid(table_id, get_key_data_vdxf_id(T_B_DECK_BV_KEY,game_id_str));
+
+		
+		b_blinded_deck = get_cJSON_from_id_key_vdxfid(table_id, get_key_data_vdxf_id(all_t_b_p_keys[player_id+1], game_id_str));
+		b_blinded_card = jbits256i(b_blinded_deck, card_id);
+		if(player_id == -1)
+			blinded_value = jbits256i(bv_info, player_id);
+		else
+			blinded_value = jbits256i(bv_info, 0);
+
+		dealer_blind_info = get_cJSON_from_id_key_vdxfid(table_id, get_key_data_vdxf_id(T_D_DECK_KEY,game_id_str));
+
+		decode_card(b_blinded_card, blinded_value, dealer_blind_info);
+	}
+}
+
+int32_t handle_game_state_player(char *table_id)
+{
+	int32_t game_state, retval = OK;
+
+	game_state = get_game_state(table_id);
+	dlg_info("%s", game_state_str(game_state));
+	switch (game_state) {
+		case G_REVEAL_CARD_P:
+			reveal_card();		
+			break;
+		default:
+			dlg_info("%s", game_state_str(game_state));
+	}
+	return retval;
+}
+
+
+int32_t handle_verus_player()
+{
+	int32_t retval = OK, player_id = -1;
+
+	playing_nodes_init();
+	bet_parse_verus_player();
+
+	if ((retval = find_table()) != OK)
+		return retval;
+
+	dlg_info("Table found");
+	print_struct_table(&player_t);
+
+	if ((retval = join_table()) != OK)
+		return retval;
+
+	dlg_info("Table Joined");
+	if ((retval = get_player_id(&p_deck_info.player_id)) != OK) {
+		return retval;
+	}
+	dlg_info("Player ID ::%d", p_deck_info.player_id);
+
+	if ((retval = player_init_deck()) != OK) {
+		return retval;
+	}
+	dlg_info("Player deck shuffling info updated to table");
+
+	
+	while (1) {
+		handle_game_state_player(player_config.table_id);
+		sleep(2);
+	}
+	
+	return retval;
+}
+
