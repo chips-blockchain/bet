a6e5fd9cea6d74748df69d9ea0ddb7059c3a7583
sg777 <sarath.ginjupalli89@gmail.com>
2023-11-16 11:00:12 +0530

    Adding header file

--- privatebet/vdxf.c
+++ privatebet/vdxf.c
@@ -5,6 +5,7 @@
 #include "err.h"
 #include "game.h"
 #include "storage.h"
+#include "config.h"
 
 struct table player_t = { 0 };
 char T_PLAYER_KEYS[9][128] = { T_PLAYER1_KEY, T_PLAYER2_KEY, T_PLAYER3_KEY, T_PLAYER4_KEY, T_PLAYER5_KEY,
