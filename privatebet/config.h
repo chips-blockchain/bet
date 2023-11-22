38eb580c9e2e72c26b33e6a419417bedca8e4784
sg777 <sarath.ginjupalli89@gmail.com>
2023-11-22 15:46:38 +0530

    Adding parsing error check

--- privatebet/config.h
+++ privatebet/config.h
@@ -8,4 +8,5 @@ int32_t bet_parse_bets();
 void bet_parse_blockchain_config_ini_file();
 bool bet_is_new_block_set();
 int32_t bet_parse_verus_dealer();
-void bet_parse_verus_player();
+int32_t bet_parse_verus_player();
+_ver
