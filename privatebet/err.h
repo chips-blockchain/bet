1c2f2e2cd447c514c447359ecb056dae28170c45
sg777 <sarath.ginjupalli89@gmail.com>
2024-02-05 11:34:32 +0530

    Made checks whether if the primaryaddress mentioned in the player config belong to the wallet on which the player node is running

--- privatebet/err.h
+++ privatebet/err.h
@@ -122,6 +122,7 @@ All the errors that come across in bet are defined here. The error numbers are a
 #define ERR_CONFIG_BLOCKCHAIN_ARGS      135
 #define ERR_IDS_NOT_CONFIGURED			136
 #define ERR_ID_AUTH						137
+#define ERR_ADDR_AUTH					138
 
 // clang-format on
 const char *bet_err_str(int32_t err_no);
