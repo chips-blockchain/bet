afbc96a9d7303f53f6b53ece99ddb5eee5790503
sg777 <sarath.ginjupalli89@gmail.com>
2024-05-03 17:25:25 +0200

    Fixed the error in retriving the dealers

--- privatebet/player.c
+++ privatebet/player.c
@@ -146,15 +146,15 @@ int32_t handle_verus_player()
 		return ERR_ADDR_AUTH;
 	}
 
-	if(!is_id_exists(player_config.verus_pid,0)) {
+	if (!is_id_exists(player_config.verus_pid, 0)) {
 		dlg_error("The player ID configured in verus_player.ini doesn;t exist");
 		return ERR_ID_NOT_FOUND;
 	}
 
-	if(!id_cansignfor(player_config.verus_pid, 0)) {
+	if (!id_cansignfor(player_config.verus_pid, 0)) {
 		return ERR_ID_AUTH;
 	}
-		
+
 	if ((retval = find_table()) != OK) {
 		// TODO:: If retval is ERR_PA_EXISTS, i.e PA exists in the table and the player can rejoin.
 		return retval;
