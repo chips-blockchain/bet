83e79c678ad8c24c17d75f74599e2ed898e92083
sg777 <8114482+sg777@users.noreply.github.com>
2024-05-30 17:38:51 +0530

    Adding round betting

--- privatebet/game.h
+++ privatebet/game.h
@@ -9,6 +9,7 @@
 #define G_DECK_SHUFFLING_B 6
 #define G_REVEAL_CARD 7
 #define G_REVEAL_CARD_P_DONE 8
+#define G_ROUND_BETTING 9
 
 struct t_game_info_struct {
 	int32_t game_state;
@@ -24,3 +25,4 @@ cJSON *get_game_state_info(char *table_id);
 int32_t init_game_state(char *table_id);
 int32_t is_card_drawn(char *table_id);
 int32_t verus_receive_card(char *table_id, struct privatebet_vars *vars);
+int32_t verus_small_blind(char *table_id, struct privatebet_vars *vars);
